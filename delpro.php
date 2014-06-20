91bd12ba116f78da3b9f5f4c02e735ec8d4a14d6
Shubham Jain <shubhamjain1310@gmail.com>
2014-06-21 02:00:13 +0530

    Update delpro.php

--- delpro.php
+++ delpro.php
@@ -1,6 +1,6 @@
 <?php
 $id=($_POST['id']);
-$con=mysqli_connect('localhost','root','shubh','pclub_data')
+$con=mysqli_connect('localhost','root','','pclub_data')
         or die('Error connecting to MYSQL server.');
   	$result=mysqli_query($con,"SELECT * FROM project WHERE `id` = '$id'");
 	$row=mysqli_fetch_array($result);
