4a36c005579b24ac32fb8b4ae7db362d5924d7b6
Shubham Jain <shubhamjain1310@gmail.com>
2014-07-22 17:03:55 +0530

    the login and signup modal is added
    It was missing previously

--- application/views/top_head1.php
+++ application/views/top_head1.php
@@ -92,3 +92,195 @@
             ?>
 
     </div>
+    <div class="ui small modal" id="login_modal">
+      <div class="ui two column middle aligned relaxed grid basic segment">
+        <div class="column" >
+          <form action ="<?php echo base_url(); ?>site/login_validation" id="login_form" method="post" name="login">
+            <div class="ui teal inverted form segment">
+              <div class="field">
+                <div class="ui left labeled icon input" style="margin:20px 0px 10px 0px">
+                  <input name="username" type="text" value="" placeholder="Username" id="login_username">
+                  <i class="user icon"></i>
+                  <div class="ui corner label">
+                    <i class="asterisk icon"></i>
+                  </div>
+                </div>
+              </div>
+              <div class="field">
+                <div class="ui left labeled icon input">
+                  <input id="login_pass" name="password" value="" type="password" placeholder="Password" onfocus="username_check();">
+                  <i class="lock icon"></i>
+                  <div class="ui corner label">
+                    <i class="asterisk icon"></i>
+                  </div>
+                </div>
+              </div>
+              <input type="button" onfocus="password_check();" onClick="submit_login()" id="login_button" value="Login" >
+              <input type="submit" onClick="submit_login()" style="display:none;" >
+              <p style="margin-top:20px;color:red;" onclick="forgot_modal();">Forgot Password </p>
+            </div>
+          </form>
+        </div>
+  
+        <div class="column" style="height:280px; width:300px;">
+          <div style="margin:40px 0px 24px 10px; height:40px;align:center;color:red;font-size:20px;">
+              <p id="username_check"> </p>
+          </div>
+          <div style="height:40px;margin-left:10px;color:red;font-size:20px;">
+              <p id="password_check" ></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+      <div class="ui small modal" id="signup_modal">
+        <div class="ui two column middle aligned relaxed grid basic segment">
+          <div class="column" >
+            <form id="signup_form" action="<?php echo base_url(); ?>site/signup_validation" method="post" name="signup_form"> 
+              <div class="ui teal inverted form segment" >
+                <div class="field">
+                  <div class="ui left labeled icon input" style="margin:20px 0px px 0px">
+                    <input type="text" name="username" placeholder="Username" id="sign_username">
+                    <i class="user icon"></i>
+                    <div class="ui corner label">
+                      <i class="asterisk icon"></i>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="field"  >
+                  <div class="ui left labeled icon input"  >
+                    <input type="text" name="email" placeholder="Email-id" onfocus="username_sign_check();" id="sign_email">
+                    <i class="user icon"></i>
+                    <div class="ui corner label">
+                      <i class="asterisk icon"></i>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="field">
+                  <div class="ui left labeled icon input">
+                    <input id="sign_password" type="password" name="password" placeholder="Password" onfocus="email_sign_check();password_len();" onkeydown="password_len();">
+                    <i class="lock icon"></i>
+                    <div class="ui corner label">
+                      <i class="asterisk icon"></i>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="field">
+                  <div class="ui left labeled icon input">
+                    <input type="password" id="sign_cppassword" name="cppassword" placeholder="Confirm Password" onkeyup="password_cpp_check();">
+                    <i class="lock icon"></i>
+                    <div class="ui corner label">
+                      <i class="asterisk icon"></i>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="ui selection dropdown" onclick="password_sign_check();">
+                  <input id="sign_sec_que" type="hidden" name="security_ques">
+                  <div class="default text" >Security Question</div>
+                  <i class="dropdown icon" ></i>
+                  <div class="menu" >
+                    <div class="item" data-value="Place of birth">Place of birth</div>
+                    <div class="item" data-value="Favourite Movie">Favourite Movie</div>
+                    <div class="item" data-value="First Pet">First Pet</div>
+                    <div class="item" data-value="Favourite Dish">Favourite Dish</div>
+                  </div>
+                </div>
+
+                <div class="field" style="margin:10px 0px">
+                  <div class="ui left labeled icon input">
+                    <input id="sign_sec_ans" type="text" name="security_ans" placeholder="Security Ans">
+                    <i class="user icon"></i>
+                    <div class="ui corner label">
+                      <i class="asterisk icon"></i>
+                    </div>
+                  </div>
+                </div>
+                
+                <input type="submit" onClick="submit_signup()" style="display:none;" >
+                <input type="button" onClick="submit_signup()" id="signup_button" value="Signup">
+              </div>
+            </form>
+          </div>
+          
+          <div class="column" style="height:430px; width:400px;padding-top:0px;">
+              <div style="margin:0px 10px 18px 10px; height:28px;align:center;color:red;font-size:20px;padding:5px;">
+                  <p id="username_sign_check" style="margin:10px;margin-bottom:20px;"></p>
+              </div>
+              <div style="margin:10px 10px 24px 10px; height:28px;align:center;color:red;font-size:20px;padding:5px;">
+                  <p id="email_sign_check" style="margin:10px;margin-bottom:20px;"></p>
+              </div>
+              <div style="height:40px;margin:40px 10px 0px 10px;color:red;font-size:20px;padding:5px;">
+                  <p id="password_sign_check"  style="margin:10px;margin-bottom:20px;"></p>
+              </div>
+          </div>
+        </div>
+
+      </div>
+
+
+
+
+
+    <div class="ui small modal" id="forgot_modal" style="width:400px;">
+      <div class="ui teal inverted form segment">
+        <div id="forgot_top">
+          <form id="forgot_form" method="post" name="forgot_form"> 
+            <div class="field">
+              <div class="ui left labeled icon input" style="margin:20px 0px px 0px">
+                <input type="text" name="username" placeholder="Username" id="forgot_username">
+                <i class="user icon"></i>
+                <div class="ui corner label">
+                  <i class="asterisk icon"></i>
+                </div>
+              </div>
+            </div>
+
+            <div class="ui selection dropdown" >
+              <input type="hidden" name="security_ques" id="forgot_sec_que">
+              <div class="default text" >Security Question</div>
+              <i class="dropdown icon" ></i>
+              <div class="menu" >
+                <div class="item" data-value="Place of birth">Place of birth</div>
+                <div class="item" data-value="Favourite Movie">Favourite Movie</div>
+                <div class="item" data-value="First Pet">First Pet</div>
+                <div class="item" data-value="Favourite Dish">Favourite Dish</div>
+              </div>
+            </div>
+
+            <div class="field" style="margin:10px 0px">
+              <div class="ui left labeled icon input">
+                <input type="text" name="security_ans" placeholder="Security Ans" id="forgot_sec_ans">
+                <i class="user icon"></i>
+                <div class="ui corner label">
+                  <i class="asterisk icon"></i>
+                </div>
+              </div>
+            </div>
+            <input type="button" onClick="forgot_validate();" id="forgot_button" value="Continue">
+          </form>
+        </div>
+        <div class="ui horizontal divider" style="margin-top:4em;color:red">
+        Or</div>
+        <p> Send reset link to your email Id </p>
+        <form id="signin_form" action="<?php echo base_url(); ?>site/" method="post" name="forgot_form">
+          <div class="field"  >
+            <div class="ui left labeled icon input"  >
+              <input type="text" name="email" placeholder="Email-id" id="forgot_email">
+              <i class="user icon"></i>
+              <div class="ui corner label">
+                <i class="asterisk icon"></i>
+              </div>
+            </div>
+          </div>
+          <input type="button" onClick="send_email_forgot()" id="forgot_button" value="Continue">
+        </form>
+      </div>
+    </div>
